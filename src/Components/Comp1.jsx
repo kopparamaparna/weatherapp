f82034f48f073db3a3b19c82c1bb1b86f91034f9
kopparam Lakshmi Aparna <103281325+kopparamaparna@users.noreply.github.com>
2024-03-28 23:56:24 +0530

    Update Comp1.jsx

--- src/Components/Comp1.jsx
+++ src/Components/Comp1.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import './Comp1.css';
-
 const Comp1 = () => {
     const [location, setLocation] = useState('');
     const [weatherData, setWeatherData] = useState(null);
